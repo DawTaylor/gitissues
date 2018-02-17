7c2ba126bb9a7c7a98ed61f624f5af6b5e2abe58
Adalberto Taylor <adalbertotaylor84@gmail.com>
2018-02-16 23:25:18 -0200

    Added Material Ui provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+)
+
+ReactDOM.render(root, document.querySelector('#root'));
