6beca87b7e64e54d7e12e363572dd0c31c920ac5
Adalberto Taylor <adalbertotaylor84@gmail.com>
2018-02-19 14:44:21 -0300

    Applyed color scheme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import App from './App'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import { App } from './App'
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#333333',
+    accent1Color: '#BDBDBD',
+    secondaryTextColor: '#333333'
+  }
+})
 
 const root = (
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={muiTheme}>
     <App />
   </MuiThemeProvider>
 )
